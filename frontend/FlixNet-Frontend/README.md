3629c6d5cd8568a710ad7610bb35b7a8751e5d10
alejandrovelez0208 <alejandrovelezarboleda0208@outlook.com>
2026-07-21 19:12:05 -0500

    Feat: Main page is created

--- frontend/FlixNet-Frontend/README.md
+++ frontend/FlixNet-Frontend/README.md
@@ -1,2 +1,3 @@
+**21-07-2026**: Main page is created //8:02:14
 **20-07-2026**: Landing page is implemented //7:36:17
 **20-07-2026**: The Angular proyect is added
--- frontend/FlixNet-Frontend/src/app/landing/landing.ts
+++ frontend/FlixNet-Frontend/src/app/landing/landing.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,6 +9,72 @@ import { Component } from '@angular/core';
   styleUrl: './landing.css',
 })
 export class Landing {
+  landingForm!: FormGroup;
+  year = new Date().getFullYear();
 
-  login(){}
+  constructor(private fb: FormBuilder, private route: Router) {
+    this.landingForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  login() {
+    this.route.navigate(['/login'])
+
+
+  }
+
+  getStarted() {
+    this.route.navigate(['/signup'], { queryParams: { email: this.landingForm.value } })
+  }
+
+  reasons = [
+    {
+      title: 'Enjoy on your TV',
+      text: 'Watch on smart TVs, PlayStation, xbox, players and more.',
+      icon: 'tv'
+    },
+    {
+      title: 'Download your shows to watch offline',
+      text: 'Save your favourites easily and always have something to watch.',
+      icon: 'file_download'
+    },
+    {
+      title: 'Watch everywhere',
+      text: 'Stream unlimited movies and TV shows on your phone, tablet, laptop and TV.',
+      icon: 'devices'
+    },
+    {
+      title: 'Create profiles for kids',
+      text: 'Send kids on adventures in a space made just for them - free with your membership.',
+      icon: 'face'
+    }
+  ]
+
+  faqs = [
+    {
+      question: 'What is PulseScreen?',
+      answer: 'PulseScreen is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more.'
+    },
+    {
+      question: 'How much does PulseScreen cost?',
+      answer: 'Plans start at ₹149 a month. No extra costs, no contracts.'
+    },
+    {
+      question: 'Where can I watch?',
+      answer: 'Watch anywhere, anytime. Sign in with your account to watch on the web or on devices like smartphones, tablets, smart TVs and...'
+    },
+    {
+      question: 'How do I cancel?',
+      answer: 'You can cancel your membership online in two clicks. There are no cancellation fees – start or stop your account anytime.'
+    },
+    {
+      question: 'What can I watch on PulseScreen?',
+      answer: 'A huge library of feature films, documentaries, anime, TV shows, PulseScreen originals and more.'
+    },
+    {
+      question: 'Is PulseScreen good for kids?',
+      answer: 'The Kids experience includes family-friendly entertainment with parental controls to restrict content by maturity rating.'
+    }
+  ]
 }
